369f6c9a1a6d3be8f214e1e67d5b2e90fd7159d0
Eliasz Sawicki <eliasz.sawicki@gmail.com>
2017-06-09 18:14:29 +0200

    save

--- BrightOfficer/UsersFetcher.swift
+++ BrightOfficer/UsersFetcher.swift
@@ -16,13 +16,14 @@ class UsersFetcher {
     }
     
     func fakeUsers() -> [User] {
+        let gdansk = CLLocationCoordinate2D(latitude: 54.382283, longitude: 18.605526)
         let pic = "https://gravatar.com/avatar/55502f40dc8b7c769880b10874abc9d0?d=identicon"
-        let user1 = User(name: "Mark Twain", picture: pic, location: CLLocationCoordinate2D(latitude: 1,longitude: 2), phoneNumber: "123456789", weight: 90, info: "Wózek inwalidzki")
-        let user2 = User(name: "Danny Cowey", picture: pic, location: CLLocationCoordinate2D(latitude: 1,longitude: 2), phoneNumber: "123456789", weight: 85, info: "Foreign speaker")
-        let user3 = User(name: "Paul Wiss", picture: pic, location: CLLocationCoordinate2D(latitude: 1,longitude: 2), phoneNumber: "123456789", weight: 70, info: "Wózek inwalidzki")
-        let user4 = User(name: "Danny Core", picture: pic, location: CLLocationCoordinate2D(latitude: 1,longitude: 2), phoneNumber: "123456789", weight: 56, info: "Duża torba")
-        let user5 = User(name: "Terry Patch", picture: pic, location: CLLocationCoordinate2D(latitude: 1,longitude: 2), phoneNumber: "123456789", weight: 120, info: "Wózek inwalidzki")
-        let user6 = User(name: "Iggy Pial", picture: pic, location: CLLocationCoordinate2D(latitude: 1,longitude: 2), phoneNumber: "123456789", weight: 15, info: "Wózek inwalidzki")
+        let user1 = User(name: "Mark Twain", picture: pic, location: gdansk, phoneNumber: "123456789", weight: 90, info: "Wózek inwalidzki")
+        let user2 = User(name: "Danny Cowey", picture: pic, location: gdansk, phoneNumber: "123456789", weight: 85, info: "Foreign speaker")
+        let user3 = User(name: "Paul Wiss", picture: pic, location: gdansk, phoneNumber: "123456789", weight: 70, info: "Wózek inwalidzki")
+        let user4 = User(name: "Danny Core", picture: pic, location: gdansk, phoneNumber: "123456789", weight: 56, info: "Duża torba")
+        let user5 = User(name: "Terry Patch", picture: pic, location: gdansk, phoneNumber: "123456789", weight: 120, info: "Wózek inwalidzki")
+        let user6 = User(name: "Iggy Pial", picture: pic, location: gdansk, phoneNumber: "123456789", weight: 15, info: "Wózek inwalidzki")
         
         
         return [user1, user2, user3, user4, user5, user6]
