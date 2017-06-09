9a6e50f3197874c2677bade6e8dde101af4d0f49
Eliasz Sawicki <eliasz.sawicki@gmail.com>
2017-06-09 17:09:45 +0200

    save

---
+++ BrightOfficer/User.swift
@@ -0,0 +1,21 @@
+//
+//  User.swift
+//  BrightOfficer
+//
+//  Created by Eliasz Sawicki on 09/06/2017.
+//  Copyright © 2017 Eliasz Sawicki. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class User {
+    
+    var name: String = ""
+    var picture: String?
+    var location: CLLocationCoordinate2D?
+    var phoneNumber: String = ""
+    var weight: Double = 0.0
+    var info: String = ""
+    
+}
