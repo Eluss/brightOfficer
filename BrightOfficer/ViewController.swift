1581a0f8205ecf5795177189504964b352b9da6c
Eliasz Sawicki <eliasz.sawicki@gmail.com>
2017-06-09 20:37:52 +0200

    save

--- BrightOfficer/ViewController.swift
+++ BrightOfficer/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Bright Officer"
-        tableView.backgroundColor = UIColor(colorLiteralRed: 50, green: 50, blue: 50, alpha: 1)
+        UITableView.appearance().backgroundColor = UIColor(colorLiteralRed: 50, green: 50, blue: 50, alpha: 1)
         usersFetcher.fetchUsers { users in
             self.users = users
             self.tableView.reloadData()
